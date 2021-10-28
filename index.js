44199082bea82790d7fe5345d16d9713fed0ebcc
Max Botwin <maxmarbot@gmail.com>
2021-10-28 17:29:06 -0500

    final

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function printNumbers(numbers) {
 }
 
 // Q: Given an array of numbers, what is the space complexity of this function?
-// ANSWER:
+// ANSWER: O(n)
 
 
 // 2) You are challenged to find the 4th element of the array, and you came up with this solution:
@@ -27,7 +27,7 @@ function findFourth(numbers) {
 }
 
 // Q: Given an array of numbers, what is the space complexity of this function?
-// ANSWER:
+// ANSWER: O(n)
 
 
 // 3) -------------------------------------------------------
@@ -37,7 +37,7 @@ function printA() {
     }
 }
 // Q: What is the space complexity of this function?
-// ANSWER:
+// ANSWER:O(1)
 
 
 // 4) -------------------------------------------------------
@@ -47,7 +47,7 @@ function printB(number) {
   }
 }
 // Q: What is the space complexity of the function?
-// ANSWER:
+// ANSWER: O(1)
 
 
 // 5) -------------------------------------------------------
@@ -59,7 +59,7 @@ function printC(num, arr) {
   }
 }
 // Q: What is the space complexity of the function?
-// ANSWER:
+// ANSWER: O(n)
 
 
 // 6) -------------------------------------------------------
@@ -72,7 +72,7 @@ function nested(numbers) {
 }
 
 // Q: What is the space complexity of this function?
-// ANSWER:
+// ANSWER:O(n^2)
 
 
 // 7) -------------------------------------------------------
@@ -83,7 +83,7 @@ let newNumbersA = numbers.filter((num) => {
 })
 
 // Q: What is the space complexity of the filter higher-order function?
-// ANSWER:
+// ANSWER:O(n)
 
 
 // 8) -------------------------------------------------------
@@ -92,7 +92,7 @@ let newNumbersB = numbers.map((num) => {
   return num * 3
 })
 // Q: What is the space complexity of the map higher-order function?
-// ANSWER:
+// ANSWER:O(n)
 
 
 // 9) -------------------------------------------------------
@@ -109,7 +109,7 @@ function arrInception(parentArr) {
   }
 }
 // Q: What is the space complexity of this function?
-// ANSWER:
+// ANSWER:O(n)
 
 
 // 10) You are challenged to find the squared value of each number in an array. This is not the best solution, but it is what you come up with anyway...
@@ -127,11 +127,11 @@ function getNumbersSquared(numbers) {
 }
 
 // Q: What is the space complexity of this function?
-// ANSWER:
+// ANSWER:O(n)
 
 
 // Q: Is it possible to improve this function's space complexity? If it is, write a new function below.
-// ANSWER:
+// ANSWER: No
 
 
 // 11) Here are two functions that you came up with to count how many of each letter is in a given string and return an object with the individual counts: 
@@ -196,16 +196,16 @@ function countLettersRoundTwo(str) {
 
 
 // Q: Do these functions have the same space complexity?
-// ANSWER:
+// ANSWER: Yes, they're both linear
 
 
 // Q: What is the space complexity of each? 
-// ANSWER - countLetters:
-// ANSWER - countLettersRoundTwo:
+// ANSWER - countLetters: O(n)
+// ANSWER - countLettersRoundTwo: O(n)
 
 
 // Q: Explain how you came to your conclusions about the space complexity of each function. 
-// ANSWER: 
+// ANSWER: Both functions scale with the length of the string.
 
 
 // 12) Memoization
@@ -239,7 +239,22 @@ function countLettersRoundTwo(str) {
 */
 
 // CODE HERE 
-
+ class factorial {
+     constructor() {
+         this.cache = {}
+     }
+     calcFac (num) {
+         if (this.cache.hasOwnProperty(num)) {
+             return this.cache[num]
+         } else {
+             let answer = 0
+             for (let i = num; i > 0; i-- )
+                answer = num * i
+                this.cache[num] = answer
+                return answer
+         }
+     }
+ }
 
 /* 
   We want to test a non-memoized version of this too, 
@@ -250,7 +265,15 @@ function countLettersRoundTwo(str) {
 */
 
 // function noCacheCalcFac(num) {
-    //CODE HERE
+    if (this.cache.hasOwnProperty(num)) {
+        return this.cache[num]
+    } else {
+        let answer = 0
+        for (let i = num; i > 0; i-- )
+           answer = num * i
+           this.cache[num] = answer
+           return answer
+    }
 // }
 
 /*
@@ -260,20 +283,17 @@ function countLettersRoundTwo(str) {
   (run using 'node index.js' in the terminal)
 */
 
-const myFac = // CREATE A NEW INSTANCE OF THE FACTORIAL CLASS
+const myFac = new factorial
 
 console.time("not memoized factorial");
-console.log(/* CALL NOCACHECALCFAC PASSING IN 8*/)
-console.log(/* CALL NOCACHECALCFAC PASSING IN 8*/)
-console.log(/* CALL NOCACHECALCFAC PASSING IN 8*/)
-console.log(/* CALL NOCACHECALCFAC PASSING IN 8*/)
+console.log(nonCacheCalcFac(8))
+console.log(nonCacheCalcFac(8))
+console.log(nonCacheCalcFac(8))
+console.log(nonCacheCalcFac(8))
 console.timeEnd("not memoized factorial");
 
 console.time("memoized factorial");
-console.log(/* CALL THE CALCFAC METHOD OFF OF MYFAC 8*/)
-console.log(/* CALL THE CALCFAC METHOD OFF OF MYFAC 8*/)
-console.log(/* CALL THE CALCFAC METHOD OFF OF MYFAC 8*/)
-console.log(/* CALL THE CALCFAC METHOD OFF OF MYFAC 8*/)
+console.log(myFac.calcFac(8))
 console.timeEnd("memoized factorial");
 
 // 13) Memoization Challenge
@@ -286,7 +306,14 @@ console.timeEnd("memoized factorial");
   we've covered or come up with your own)
 */
 
-// CODE HERE
+class ansCache {
+    constructor() {
+        this.cache = {}
+    }
+    cacheAns (answers) {
+        
+    }
+}
 
 /*
     Copy and paste the method outside the class and
